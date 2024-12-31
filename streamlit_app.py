10e75231e3a05db252b0ca8e572d3876b1acc7cf
praneeth261103 <159001409+praneeth261103@users.noreply.github.com>
2024-12-31 20:49:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,11 @@ if ingredients_list:
             values('"""+ingredients_string+"""','"""+name_on_order+"""')"""
     # st.write(my_insert_stmt)
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+
     time_to_insert=st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
